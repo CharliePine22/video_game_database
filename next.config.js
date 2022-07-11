98994088d972708170e144a97e29b5653a6ade81
Cj <55663953+CharliePine22@users.noreply.github.com>
2022-07-11 18:30:38 -0500

    Dependencies

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-require("dotenv").config();
-
 module.exports = {
   reactStrictMode: true,
-  "baseUrl": ".",
-  "include": [
-      "next-env.d.ts",
-      "**/*.ts",
-      "**/*.tsx"
-    ],
-    env: {
-		DOMAIN: process.env.DOMAIN,
-		STEAM_API_KEY: process.env.STEAM_API_KEY,
-		SESSION_SECRET: process.env.SESSION_SECRET
-    }
-}
+  baseUrl: '.',
+  include: ['next-env.d.ts', '**/*.ts', '**/*.tsx'],
+  env: {
+    MONGODB_USER: process.env.NEXT_PUBLIC_MONGODB_USER,
+    MONGODB_PASSWORD: process.env.NEXT_PUBLIC_MONGODB_PASSWORD,
+    DOMAIN: process.env.DOMAIN,
+    STEAM_API_KEY: process.env.STEAM_API_KEY,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+  },
+};
