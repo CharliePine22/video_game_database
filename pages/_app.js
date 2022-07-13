72c8304ebf775d78b6ef080cc7abd0268ecc1546
Cj <55663953+CharliePine22@users.noreply.github.com>
2022-07-13 16:01:39 -0500

    Updated Home Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import NextNprogress from 'nextjs-progressbar';
 import { ResultsProvider } from '../src/contexts/ResultsContext';
-import { UserProvider } from '@auth0/nextjs-auth0';
 import NavigationBar from '../src/components/layout/navbar/NavigationBar';
 
 function MyApp({ Component, pageProps }) {
@@ -13,13 +12,11 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <UserProvider>
         <ResultsProvider>
           <NextNprogress color='#FF0000' />
           <NavigationBar />
           <Component {...pageProps} />
         </ResultsProvider>
-      </UserProvider>
     </>
   );
 }
